99f95ee832d799ab58987efcba0f3647ba0e0a0a
Chinmay Chhajed <chinmay.chhajed@espressif.com>
2020-04-23 15:01:01 +0530

    Scripts using terminal will use global $TERMINAL.
    If $TERMINAL is unset globally then urxvt will be used by default.
    
    Signed-off-by: Chinmay Chhajed <chinmay.chhajed@espressif.com>

--- startpy3.sh
+++ startpy3.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-(command -v python3 >/dev/null && urxvt -name dropdown_python -e sh -c 'python3 -q' &)
+# Default terminal is urxvt.
+[ -n "$TERMINAL" ] || export $TERMINAL="urxvt"
+(command -v python3 >/dev/null && $TERMINAL -name dropdown_python -e sh -c 'python3 -q' &)
