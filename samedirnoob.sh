d68371f051335692aed30d76335d054a1463af76
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2020-04-27 11:39:16 +0530

    Moved file to store current directory to /tmp/.
    Corresponding changes in .bashrc changing PROMPT_COMMAND.
    
    Signed-off-by: Chinmay Chhajed <chinmay.chhajed@espressif.com>

--- samedirnoob.sh
+++ samedirnoob.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# PROMPT_COMMAND="pwd > '${HOME}/.cwd'; $PROMPT_COMMAND"
+# PROMPT_COMMAND="pwd > '/tmp/cwd'; $PROMPT_COMMAND"
 # source: i3 forums, http://blog.soulshake.net/2016/03/i3-new-terminals-in-pwd/
 
 
 # Change to saved working dir
-# pwd > '${HOME}/.cwd'
-[[ -f "${HOME}/.cwd" ]] && cd "$(< ${HOME}/.cwd)" && $TERMINAL
+[[ -f "/tmp/cwd" ]] && cd "$(< /tmp/cwd)" && $TERMINAL
