bf185cf0c7494700009e5038b0078bb231aa1cf2
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2020-05-03 15:18:10 +0530

    Useless commit.

--- test.c
+++ test.c
@@ -734,10 +734,82 @@
  * }
  */
 
-#include "stdbool.h"
-int main(void)
+// A utility function to swap two elements  
+void swap(long int* a, long int* b)  
+{  
+    int t = *a;  
+    *a = *b;  
+    *b = t;  
+}  
+  
+/* This function takes last element as pivot, places  
+the pivot element at its correct position in sorted  
+array, and places all smaller (smaller than pivot)  
+to left of pivot and all greater elements to right  
+of pivot */
+int partition (long int arr[], long int low, long int high)  
+{  
+    int pivot = arr[high]; // pivot  
+    int i = (low - 1); // Index of smaller element  
+  
+    for (int j = low; j <= high - 1; j++)  
+    {  
+        // If current element is smaller than the pivot  
+        if (arr[j] < pivot)  
+        {  
+            i++; // increment index of smaller element  
+            swap(&arr[i], &arr[j]);  
+        }  
+    }  
+    swap(&arr[i + 1], &arr[high]);  
+    return (i + 1);  
+}  
+  
+/* The main function that implements QuickSort  
+arr[] --> Array to be sorted,  
+low --> Starting index,  
+high --> Ending index */
+void sort(long int arr[], long int low, long int high)
 {
-    bool a;
-    if (a > 0)
-        printf("%d\n", a);
+    if (low < high)  
+    {  
+        /* pi is partitioning index, arr[p] is now  
+        at right place */
+        int pi = partition(arr, low, high);  
+  
+        // Separately sort elements before  
+        // partition and after partition  
+        sort(arr, low, pi - 1);  
+        sort(arr, pi + 1, high);  
+    }  
+}  
+  
+#define MAX 100000
+int main (void)
+{
+    long int t, n, h[MAX], v[MAX];
+    scanf("%ld", &t);
+    while(t--) {
+        scanf("%ld", &n);
+        for (long int i = 0; i < n; i += 1) {
+            scanf("%ld", &h[i]);
+        }
+        for (long int i = 0; i < n; i += 1) {
+            scanf("%ld", &v[i]);
+        }
+        sort(h, 0, n-1);
+        sort(v, 0, n-1);
+        long int i = 0, j = 0, ans = 0;
+        while (i < n && j < n) {
+            if (h[i] > v[j]) {
+                ans += 1;
+                i += 1;
+                j += 1;
+            } else {
+                i += 1;
+            }
+        }
+        printf("%ld\n", ans);
+    }
 }
+
