e3e7d2e1210053723fd0a3efd5d88ae8e62709c5
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2020-05-05 19:21:17 +0530

    Script to restart dwmbar. Also indicators for Caps/Num Lock.
    Signed-off-by: Chinmay Chhajed <chinmay.chhajed@espressif.com>

--- volume_change.sh
+++ volume_change.sh
@@ -24,7 +24,4 @@ case $1 in
 
 esac && paplay $HOME/.local/share/supplements/volume_change.ogg
 
-# Get count of "dwmbar" processes running. If count is 1 then kill sleep
-# subprocess within it. If count is more than 1, kill all instances (if any)
-# and start a new process.
-{ [ $(pgrep -c dwmbar) = "1" ] && pkill -P $(pgrep dwmbar) sleep; } || { pkill dwmbar; dwmbar; }
+restartbar.sh
