ef39f17ad2c1ebe7d244c274e8e5ebc4e080cac9
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2020-05-15 15:20:12 +0530

    Open with samedirnoob.sh
    Currently this script is terminal dependent to work. Needs to be updated
    to be generic.
    
    Also see this: https://github.com/schischi/xcwd/issues/23

--- samedir.sh
+++ samedir.sh
@@ -1,11 +1,38 @@
 #!/bin/sh
 
 [ -n "$TERMINAL" ] || export TERMINAL="urxvt"
+
+# ID_FOCUSED=$(xdotool getwindowfocus)
+# ID_ACTIVE=$(xdotool getactivewindow)
+# PID=$(xdotool getwindowpid "$ID_ACTIVE")
+# echo "ID_FOCUSED: $ID_FOCUSED    ID_ACTIVE: $ID_ACTIVE    PID: $PID"
+
+samedirnoob.sh
+exit
+
 ID=$(xdpyinfo | grep focus | cut -f4 -d " ")
 PID=$(($(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3) + 2))
+
+echo "PID is $PID."
+
 if [ -e "/proc/$PID/cwd" ]
 then
-    $TERMINAL -cd $(readlink /proc/$PID/cwd) &
+    case "$TERMINAL" in
+        "urxvt")
+            $TERMINAL -cd "$(readlink /proc/$PID/cwd)"
+            ;;
+        "konsole")
+            # $TERMINAL --workdir "$(readlink /proc/$PID/cwd)"
+            cd "$(readlink /proc/$PID/cwd)" && $TERMINAL
+            ;;
+        "st")
+            cd "$(readlink /proc/\"$((($PID+1)))\"/cwd)" && $TERMINAL
+            ;;
+        *)
+            echo "Running default terminal or urxvt."
+            $TERMINAL
+            ;;
+    esac &
 else
     $TERMINAL
 fi
