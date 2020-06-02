8aebb06d036a9461541e90a71baae75515f0823a
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2020-06-03 00:18:52 +0530

    Now open editor by running script with arguments.
    (Pretty useless commit, but why not?)

--- editor.sh
+++ editor.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
-# Opens blank file in emacs if emacs is installed or in vi.
-# You need to run `emacs --daemon` for `emacsclient` to work which
-# will open faster than normal emacs.
+# Opens editor based on input argument.
+#     -e for emacsclient or emacs
+#     -v for Vi
+# Default behavior:
+#    Opens blank file in emacs if emacs is installed or in vi.
+#    You need to run `emacs --daemon` for `emacsclient` to work which
+#    will open faster than normal emacs.
 
-{ { type emacs >/dev/null && { emacsclient -c || emacs; }; } ||
-    { [ -n "$TERMINAL" ] || TERMINAL="urxvt"; $TERMINAL -e vi; }; }&
+case "$1" in
+
+    -e)
+	emacsclient -c || emacs
+	;;
+
+    -v)
+	[ -n "$TERMINAL" ] || TERMINAL="urxvt"; $TERMINAL -e vi
+	;;
+    *)
+	{ { type emacs >/dev/null && { emacsclient -c || emacs; }; } ||
+	      { [ -n "$TERMINAL" ] || TERMINAL="urxvt"; $TERMINAL -e vi; }; }&
+esac
