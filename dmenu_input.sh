38058c292d00db6420e56a2b0e2573cf708692d7
Chinmay Chhajed <chinmay.chhajed@espressif.com>
2020-06-18 23:16:41 +0530

    Dmenu open git repo in fastest way and also any directory.

--- dmenu_input.sh
+++ dmenu_input.sh
@@ -6,7 +6,9 @@
 # dmenu_input.sh -d: Do a dictionary search using dmenu.
 # dmneu_input.sh -m: Search for a manpage.
 # dmneu_input.sh -g: Do a google search in firefox.
-# dmneu_input.sh -r: Open a git repo stored anywhere after `$HOME'.
+# dmneu_input.sh -r: Open a git repo stored in `$HOME'.
+# dmenu_input,sh -f: Open a terminal is a directory. Similar to fuzzy
+#                    finder through dmenu but for directory instead of files.
 
 DMENU_FONT1="Inconsolata 12"
 
@@ -24,7 +26,24 @@ case $1 in
         fi
         ;;
 
-# Search google.
+# Open terminal in a directory.
+        -f)
+        DIR="$(find $HOME/ -type d -not -path '*/\.git/*' -not -path '*/\.git' 2>/dev/null |
+            sed s:/home/$USER:~: | dmenu -i -l 20 -p "Go to directory:" -fn '$DMENU_FONT1')"
+
+        if [ "$DIR" = "~/" ]; then
+             cd $HOME && $TERMINAL &
+         else
+             # Cut the '~/' part from the `DIR'.
+             DIR="$(echo $DIR | cut -d '/' -f2-)"
+
+            # If user selected any directory then open `$TERMINAL' in that directory.
+            [ "$DIR" = "" ] || { cd $HOME/$DIR && $TERMINAL; } &
+        fi
+        ;;
+
+
+# Search google or open a URL.
     -g)
         SEARCHURL='https://www.google.com/search?q='
         GOTOURL='https://'
@@ -34,13 +53,14 @@ case $1 in
             (echo $QUERY | grep ' ' >/dev/null && $BROWSER "${SEARCHURL}${QUERY}" && echo case 1) ||
                 (echo $QUERY | grep '\.' >/dev/null && $BROWSER "${GOTOURL}${QUERY}" && echo case 2) ||
                 ($BROWSER "${SEARCHURL}${QUERY}" && echo case 3)
-        fi
-        ;;
+    fi
+    ;;
 
 # Open terminal in a git repo.
     -r)
-        # Display all the git repos in `$HOME'.
-        REPOS="$(locate -r '/home/'$USER'.*\.git$' | xargs dirname  | sed s:/home/$USER:~: | dmenu -i -l 20 -p 'Select git repo to open' -fn '$DMENU_FONT1')"
+        mkdir -p $HOME/.tmp
+        [ -e $HOME/.tmp/gitfiles ] || find $HOME/ -regex .*/\.git$ -type d 2>/dev/null > $HOME/.tmp/gitfiles
+        REPOS="$(cat $HOME/.tmp/gitfiles | xargs dirname  | sed s:/home/$USER:~: | dmenu -i -l 20 -p 'Select git repo to open' -fn '$DMENU_FONT1')"
 
         # Cut the '~/' part from the `REPOS'.
         REPOS="$(echo $REPOS | cut -d '/' -f2-)"
@@ -59,11 +79,11 @@ case $1 in
             $TERMINAL -name dropdown_manual -e sh -c "man ${WORD} || figlet -c 'No manual entry for \"${WORD}\"' |less" >/dev/null
             # exec i3-msg [instance="dropdown_manual"] focus >/dev/null
         fi
-        ;;
+    ;;
 
     *)
         MESSAGE="Not a valid option to run. :P"
         $TERMINAL -name dropdown_default -e sh -c "figlet -c ${MESSAGE} |less" >/dev/null
-        ;;
+    ;;
 
 esac
