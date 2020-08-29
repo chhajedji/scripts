f80c06cbb6cf48fb4b58daedddee447d554637f3
Chinmay Chhajed <chinmay.chhajed@espressif.com>
2020-08-29 09:36:53 +0530

    Configure 2 monitors up and down.

--- monitor_config.sh
+++ monitor_config.sh
@@ -12,13 +12,16 @@ case "$1" in
 
     -two)
         # Two monitors.
-        # xrandr --output VIRTUAL1 --off --output eDP1 --mode 1600x900 --pos 0x92 --rotate normal --output DP1 --off --output HDMI2 --primary --mode 1920x1080 --pos 1600x0 --rotate normal --output HDMI1 --off --output DP2 --off && echo "Configuring 2 displays."
         xrandr --output eDP1 --mode 1600x900 --pos 1920x90 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off && echo "Configuring 2 displays."
+        ;;
+    -up)
+        # HDMI2 up, monitor down.
+        xrandr --output eDP1 --mode 1600x900 --pos 160x1080 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
         ;;
 
     *)
         # Screen mirror if second screen is connected or else for single screen.
-        xrandr | grep 'HDMI2 connected' && xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off || xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off && echo "Configuring single display."
+        xrandr | grep 'HDMI2 connected' && xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off  && echo "Duplicating display." || { xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off && echo "Configuring single display."; }
         ;;
 
     esac
