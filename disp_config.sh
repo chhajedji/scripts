95a8562ecbea3732f1d5d25800d665ce29ddff11
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2020-09-10 17:47:57 +0530

    monitor_config.sh -> disp_config.sh

--- disp_config.sh
+++ disp_config.sh
@@ -21,7 +21,7 @@ case "$1" in
 
     *)
         # Screen mirror if second screen is connected or else for single screen.
-        xrandr | grep 'HDMI2 connected' && xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off  && echo "Duplicating display." || { xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off && echo "Configuring single display."; }
+        xrandr --output eDP1 --mode 1600x900 --pos 1920x90 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off && echo "Configuring 2 displays." || { xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off && echo "Configuring single display."; }
         ;;
 
     esac
