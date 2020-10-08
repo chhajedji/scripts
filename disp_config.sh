37827f80de25ff04b105f51c39cbe08dd97ba2ca
Chinmay Chhajed <chinmay.chhajed@espressif.com>
2020-10-08 16:50:06 +0530

    Smarter and easy understanding for managing monitors.

--- disp_config.sh
+++ disp_config.sh
@@ -2,26 +2,35 @@
 
 xrandr --auto
 
+DUPLICATE="xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off"
+
+LAPTOPDOWN="xrandr --output eDP1 --mode 1600x900 --pos 160x1080 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
+
+LAPTOPRIGHT="xrandr --output eDP1 --mode 1600x900 --pos 1920x90 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
 case "$1" in
 
     -dup)
         # Duplicate monitors.
-        xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off &&
+        $DUPLICATE &&
             echo "Duplicating two monitors."
         ;;
 
     -two)
         # Two monitors.
-        xrandr --output eDP1 --mode 1600x900 --pos 1920x90 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off && echo "Configuring 2 displays."
+        $LAPTOPRIGHT && echo "Configuring 2 displays."
         ;;
     -up)
         # HDMI2 up, monitor down.
-        xrandr --output eDP1 --mode 1600x900 --pos 160x1080 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
+        $LAPTOPDOWN && echo "Configuring 2 displays."
         ;;
 
     *)
-        # Screen mirror if second screen is connected or else for single screen.
-        xrandr --output eDP1 --mode 1600x900 --pos 1920x90 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off && echo "Configuring 2 displays." || { xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off && echo "Configuring single display."; }
+        # Check if laptop lid is open or close. Act according to that.
+        # If lid is open, configure right-left display, or else duplicate.
+
+        cat /proc/acpi/button/lid/LID/state | grep "open" &&
+            $LAPTOPRIGHT && echo "Configuring 2 displays." ||
+            { $DUPLICATE && echo "Configuring single/duplicate display."; }
         ;;
 
     esac
