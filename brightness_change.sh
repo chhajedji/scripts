645506c893e1166e74a351b73faae8ce997c847e
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2020-10-18 09:19:51 +0530

    Made disp_config.sh more readable.

--- brightness_change.sh
+++ brightness_change.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 CHANGE=15
-# Argument expected is "-inc" or -"dec".
+# Argument expected is "-inc" or "-dec".
 
 xbacklight "$1" $CHANGE
 echo "Brightness changed by $CHANGE."
--- disp_config.sh
+++ disp_config.sh
@@ -7,6 +7,9 @@ DUPLICATE="xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0
 LAPTOPDOWN="xrandr --output eDP1 --mode 1600x900 --pos 160x1080 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
 
 LAPTOPRIGHT="xrandr --output eDP1 --mode 1600x900 --pos 1920x90 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
+
+ONE="xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off"
+
 case "$1" in
 
     -dup)
@@ -28,9 +31,19 @@ case "$1" in
         # Check if laptop lid is open or close. Act according to that.
         # If lid is open, configure right-left display, or else duplicate.
 
-        cat /proc/acpi/button/lid/LID/state | grep "open" &&
-            $LAPTOPRIGHT && echo "Configuring 2 displays." ||
-            { $DUPLICATE && echo "Configuring single/duplicate display."; }
+            cat /proc/acpi/button/lid/LID/state | grep 'open' >/dev/null
+            STATE="$?"
+            if [ "$STATE" = '0' ]; then
+                xrandr | grep 'HDMI. connected' >/dev/null
+                STATE="$?"
+                if [ "$STATE" = '0' ]; then
+                    $LAPTOPRIGHT && echo "Configuring 2 displays."
+                else
+                    $ONE && echo "Configuring single display."
+                fi
+            else
+                $DUPLICATE && echo "Configuring duplicate display."
+            fi
         ;;
 
     esac
