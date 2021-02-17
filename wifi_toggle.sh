7577a8d308b50d93eecd7814ff39241e9aaea97c
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2021-02-17 11:30:29 +0530

    Prints in wifi_toggle

--- wifi_toggle.sh
+++ wifi_toggle.sh
@@ -4,6 +4,8 @@ wifi="$(nmcli r wifi | awk 'FNR = 2 {print $1}')"
 if [ $wifi = "enabled" ]
 then
     nmcli r wifi off
+    echo "WiFi Turned Off."
 else
     nmcli r wifi on
+    echo "WiFi Turned On."
 fi
