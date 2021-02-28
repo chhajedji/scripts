ecd087431e004994a3d073063570883fb07de63c
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2021-02-28 18:32:41 +0530

    app_name.sh will work interactively if executed from a terminal.

--- app_name.sh
+++ app_name.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-xprop -id $(xdotool getactivewindow) | grep _NET_WM_NAME | cut -d'"' -f2
-# xprop -id $(xdotool getactivewindow) | grep 'WM_NAME(STRING)' | cut -d'"' -f2
+# If launched from terminal, will ask to select a window. Or else active
+# window's name will be displayed.
+
+if [ $(tty) = "not a tty" ]; then
+    xprop -id $(xdotool getactivewindow) | grep _NET_WM_NAME | cut -d'"' -f2
+    # xprop -id $(xdotool getactivewindow) | grep 'WM_NAME(STRING)' | cut -d'"' -f2
+else
+    echo "Select the window."
+    xprop | grep _NET_WM_NAME | cut -d'"' -f2
+fi
