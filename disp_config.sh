80db29cef83d8d051fa5231ccf9f61d7d011c02b
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2021-03-24 17:01:17 +0530

    Useless commit 😴.

--- disp_config.sh
+++ disp_config.sh
@@ -10,6 +10,8 @@ LAPTOPRIGHT="xrandr --output eDP1 --mode 1600x900 --pos 1920x90 --rotate normal 
 
 ONE="xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off"
 
+LAPTOPLEFT="xrandr --output eDP1 --mode 1600x900 --pos 0x90 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 1600x0 --rotate normal --output VIRTUAL1 --off"
+
 case "$1" in
 
     -dup)
--- new_wall.sh
+++ new_wall.sh
@@ -5,16 +5,16 @@
 
 # ls -R $HOME/Pictures/wallpapers/ | sed "s/.*\(\.[a-z]*$\)/\1/p" -n | sort | uniq
 
-IMG="$(ls -R $HOME/Pictures/wallpapers/ | grep '.*\.png\|.*\.jpg\|\.jpeg' | shuf -n 1 | xargs -I {} fd {} $HOME/Pictures/wallpapers/ )"
+# IMG="$(ls -R $HOME/Pictures/wallpapers/ | grep '.*\.png\|.*\.jpg\|\.jpeg' | shuf -n 1 | xargs -I {} fd {} $HOME/Pictures/wallpapers/ )"
 
 echo "Setting new wallpaper."
 #
-feh $IMG --bg-scale
+# feh $IMG --bg-scale
 #
-# Setting terminal colorscheme with `pywal'.
-wal -n -i "$IMG" -a 82
-
-# To set wallpaper by `feh' without `IMG' variable.
-# feh --recursive --bg-scale --randomize $HOME/Pictures/wallpapers/*
+# Set terminal colorscheme with `pywal'.
+# wal -n -i "$IMG" -a 82
 
 disp_config.sh
+# To set wallpaper by `feh' without `IMG' variable.
+feh --recursive --bg-scale --randomize $HOME/Pictures/wallpapers/*
+
