eba654c938711d280da1efe3e76b06c21cbd0ada
Chinmay Chhajed <chinmay.chhajed@espressif.com>
2021-06-18 10:17:53 +0530

    blur_lock: Don't overlay any image on lock screen.

--- blur_lock.sh
+++ blur_lock.sh
@@ -2,10 +2,14 @@
 
 # Credits: https://github.com/petvas/i3lock-blur
 TMPBG=/tmp/screen.png
-LOCK=$HOME/.local/share/supplements/lock2.png
+LOCK=$HOME/.local/share/supplements/lock.png
 RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
- 
-ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=10:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
+
+# Without any lock image
+ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -filter_complex "boxblur=10:1" -vframes 1 $TMPBG -loglevel quiet
+
+# Use image in `LOCK` and overlay it over the blurred image.
+# ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=10:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
 xset dpms 3
 xset dpms force off
 pgrep dunst >/dev/null && notify-send "DUNST_COMMAND_PAUSE" &&
