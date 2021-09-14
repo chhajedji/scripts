4e8b3b3d548438726b96e883fc8658f9ac6baee9
Chinmay Chhajed <chhajedchinmay@gmail.com>
2021-09-14 11:17:59 +0530

    new_wall: Exclude hidden directories and files.

--- new_wall.sh
+++ new_wall.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-# Usage: new_wall.sh [OPTIONAL DIRECTORY] [OPTIONAL FILE]
+# Usage: new_wall.sh [DIRECTORY|FILE]
+
+# Demo video: https://youtu.be/11Dqat5XS9c
+
 # Randomize a file from the optional argument DIRECTORY or set optional
 # argument FILE as a wallpaper on all desktops.
 
@@ -9,22 +12,28 @@
 # wal -n -i "$IMG" -a 82
 
 # First update the monitor settings before changing wallpaper.
+# Get this script here: https://github.com/chhajedji/scripts/blob/master/disp_config.sh
 disp_config.sh
 
-# Give any file as argument to set it as wallpaper.
-if [ $1 ]; then
+# Give any file or directory as argument to set it as wallpaper.
+if [ "$1" ]; then
+
+    # Check if input is directory.
     if [ -d "$1" ]; then
         echo "Setting wallpaper from $(realpath $1)"
-        feh --recursive --bg-scale --randomize "$1"
-        exit $?
+
+        feh --recursive --bg-scale --randomize "$1"/*
+        exit
     fi
+
+    # Input is a wallpaper file.
     echo "Setting $1 as new wallpaper."
     feh --bg-scale "$1"
-    exit $?
+    exit
 fi
 
 echo "Setting new wallpaper."
 
 # To set wallpaper by `feh' without `IMG' variable.
-feh --recursive --bg-scale --randomize $HOME/Pictures/wallpapers/*
+feh --recursive --bg-scale --randomize "$HOME"/Pictures/wallpapers/*
 
