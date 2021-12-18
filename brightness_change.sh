b1604109e55b4453d53cdf3311733c824649becd
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2021-12-19 01:19:07 +0530

    Welcome new laptop, for a change not a ThinkPad.

--- brightness_change.sh
+++ brightness_change.sh
@@ -3,5 +3,5 @@
 CHANGE=15
 # Argument expected is "-inc" or "-dec".
 
-xbacklight "$1" $CHANGE
-echo "Brightness changed by $CHANGE."
+xbacklight "$1" $CHANGE &&
+    echo "Brightness changed by $CHANGE."
