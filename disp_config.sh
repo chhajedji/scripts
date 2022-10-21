009e68e19498be0c2c079428a4ead86cd44309f2
Chinmay Chhajed <42715380+chhajedji@users.noreply.github.com>
2022-10-21 00:10:39 -0400

    Different systems uses different naming conventions 😮‍💨

--- disp_config.sh
+++ disp_config.sh
@@ -2,17 +2,19 @@
 
 xrandr --auto
 
-DUPLICATE="xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI1 --off --output DP2 --off"
+DUPLICATE="xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
 
-LAPTOPDOWN="xrandr --output eDP1 --mode 1600x900 --pos 160x1080 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
+LAPTOPDOWN="xrandr --output eDP1 --mode 1600x900 --pos 160x1080 --rotate normal --output DP1 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
 
-LAPTOPRIGHT="xrandr --output eDP1 --mode 1600x900 --pos 1920x90 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
+LAPTOPRIGHT="xrandr --output eDP1 --mode 1600x900 --pos 1920x90 --rotate normal --output DP1 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
 
-ONE="xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --off --output VIRTUAL1 --off"
+ONE="xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP1 --off --output HDMI1 --off --output VIRTUAL1 --off"
 
-LAPTOPLEFT="xrandr --output eDP1 --mode 1600x900 --pos 0x90 --rotate normal --output DP1 --off --output DP2 --off --output HDMI1 --off --output HDMI2 --primary --mode 1920x1080 --pos 1600x0 --rotate normal --output VIRTUAL1 --off"
+LAPTOPLEFT="xrandr --output eDP1 --mode 1600x900 --pos 0x90 --rotate normal --output DP1 --off --output HDMI1 --mode 1920x1080 --pos 1600x0 --rotate normal --output VIRTUAL1 --off"
 
 INVERTED="xrandr --output eDP1 --mode 1920x1080 --pos 0x0 --rotate inverted --output DP1 --off --output HDMI1 --off --output VIRTUAL1 --off"
+
+LEFTINVERTED="xrandr --output eDP1 --mode 1600x900 --pos 1080x554 --rotate normal --output DP1 --off --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate right --output VIRTUAL1 --off"
 
 case "$1" in
 
@@ -45,7 +47,7 @@ case "$1" in
         # Check if laptop lid is open or close. Act according to that.
         # If lid is open, configure right-left display, or else duplicate.
 
-            cat /proc/acpi/button/lid/LID/state | grep 'open' >/dev/null
+            cat /proc/acpi/button/lid/LID0/state | grep 'open' >/dev/null
             STATE="$?"
             if [ "$STATE" = '0' ]; then
                 xrandr | grep 'HDMI. connected' >/dev/null
